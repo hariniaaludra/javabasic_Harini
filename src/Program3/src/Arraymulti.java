ceac7de210c50ac219375e9f27b8a8aa86cfb92e
harinirswekrish <harinirswekrish@gmail.com>
2021-10-25 19:50:12 +0530

    first commit

---
+++ src/Program3/src/Arraymulti.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Arraymulti {
+	public static void main(String[]args) {
+		int arr1[][]= {{10,20,30},{40,50,60}};
+		int arr2[][]= {{110,120,130},{140,150,160}};
+		for(int i=0;i<arr1.length;i++){
+			for(int j=0;j<arr2.length;j++)
+			{
+	
+				System.out.println(Arrays.toString(arr1[i]));
+				System.out.println(Arrays.toString(arr2[j]));
+			}
+			
+			
+		
+		}
+		
+	}
+}
+
+
---
+++ src/Program3/src/Methods.java
@@ -0,0 +1,20 @@
+
+public class Methods {
+	
+	static void myMethod() {
+		String name="this is a static method";
+		System.out.println(name);
+	}
+		public static void main(String[]args) {
+			myMethod();
+			myMethod();
+			myMethod();
+			
+			
+		}
+		
+			
+		}
+	
+
+
